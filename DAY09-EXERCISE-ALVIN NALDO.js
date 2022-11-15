ca4b891eea98fdf37fda3b2f8ab12fa6ef83b024
alvinnaldo <naldoalvin45@gmail.com>
2022-11-15 23:11:58 +0700

    Menyelesaikan exercise day 9

---
+++ DAY09-EXERCISE-ALVIN NALDO.js
@@ -0,0 +1,97 @@
+/*
+    Title : Function to reverse string
+    Input : function with one parameter with data type string
+    
+    Process :   
+    1. Split the string into each word using split method
+    2. Reverse the string using reverse method
+    3. Join the reverse string of array into string using join method
+    
+    Output : return the reversed string
+*/
+
+function reverse(string) {
+  return string.split("").reverse().join("");
+}
+
+console.log(reverse("hello"));
+
+/*
+    Title : Duplicate function map
+    Input : function with two parameter --> data type array and a callback function 
+
+    Process :
+    1. Create FOR LOOP to loop all the array index
+    2. Change each element of the array with the return value from the callback function
+    3. Return the modified array
+    
+    Output : the modified array
+*/
+
+function duplicateMap(arr, cbfn) {
+  for (i = 0; i < arr.length; i++) {
+    arr[i] = cbfn(arr[i]);
+  }
+  return arr;
+}
+
+console.log(
+  duplicateMap(["bulat", "bulat", "bulat", "bulat"], (element) => "persegi")
+);
+console.log(duplicateMap([1, 3, 5], (element) => element * 2));
+
+/*
+    Title : Duplicate function filter
+    Input : function with two parameter --> data type array and a callback function 
+
+    Process :
+    1. Create an empty array to store the filtered elements
+    2. Create FOR LOOP to loop all the array index
+    3. Use conditional IF to check if the return value of the callback function has the same value with the element of the array
+    4. If the condition is true, then push the element into empty array, and then return that array
+    
+    Output : the filtered array
+*/
+
+function duplicateFilter(arr, cbfn) {
+  let array = [];
+  for (i = 0; i < arr.length; i++) {
+    if (cbfn(arr[i])) {
+      array.push(arr[i]);
+    }
+  }
+  return array;
+}
+
+console.log(
+  duplicateFilter(
+    ["bulat", "persegi", "persegi", "persegi"],
+    (element) => element === "persegi"
+  )
+);
+
+/*
+    Title : Duplicate function findIndexOf
+    Input : function with two parameter --> data type array and a callback function 
+
+    Process :
+    1. Create FOR LOOP to loop all the array index
+    2. Use conditional IF to check if the return value of the callback function has the same index with the element in the array
+    3. If the condition is true, then return the index
+    
+    Output : index of the element that we searched
+*/
+
+function duplicateFindIndexOf(arr, cbfn) {
+  for (i = 0; i < arr.length; i++) {
+    if (cbfn(arr[i])) {
+      return i;
+    }
+  }
+}
+console.log(
+  duplicateFindIndexOf(
+    ["bulat", "persegi", "segitiga", "silang"],
+    (element) => element === "segitiga"
+  )
+);
